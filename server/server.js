e3bea690ac38cd149514a42964b684d2a0a4c183
Justin Ou <justinou@Justins-MacBook-Air.local>
2016-08-08 12:17:34 -0700

    Inital push for Uncle
    Set up server for Node and empty server-config file for middleware

---
+++ server/server.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+//server config for middleware later
+// import server-config from './server-config';
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.get('/', function(req, res) {
+  res.json('Hello World')
+});
+
+app.listen(port, () => {
+  console.log('server started on port ' + port);
+});
+
+
