ecb01819b8143dcea3e9d3bffdce19681896f6a4
Billy Lan <billylan22@gmail.com>
2016-08-09 11:38:17 -0700

    Add basic conversation flow

--- server/src/bot.js
+++ server/src/bot.js
@@ -12,10 +12,32 @@ UNCLE.spawn({
 });
 // }).startRTM();
 
-UNCLE.hears('', ['direct_message'], function(bot, message) {
+UNCLE.hears("", ['direct_message'], function(bot, message) {
   console.log('replying to message');
   let responses = ["Yooo It's your boi JBuxxx, wats good?"]
-  bot.reply(message, responses[0]);
+  bot.startConversation(message, askFirstName);
 });
 
+let askFirstName = (response, convo) => {
+  convo.ask("Yo dawg, it's yo boi JBuxXx, you tryna slide in my dm's? What does yo mama call you?", (response, convo) => {
+    convo.say("Fo real? wat kinda name is " + response.text + ". She must not luv u");
+    askLastName(response, convo);
+    convo.next();
+  })
+}
+
+let askLastName = (response, convo) => {
+  convo.ask("Alright, with a terrible name like that you must have a good last name, what is it?", (response, convo) => {
+    convo.say("Dangggg you really aint lucky, might need to go get that changed legally");
+    askLocation(response, convo);
+    convo.next();
+  })
+}
+
+let askLocation = (response, convo) => {
+  convo.ask("Ok so you got a turrible name but maybe you from a place where everyone has weird names, where you from?", (response, convo) => {
+    convo.say("I heard " + response.text + " is dope, got some beautiful women over thur")
+    convo.next();
+  })
+}
 
