17ce20f34c88a3687185f98702912e283750ac98
Billy Lan <billylan22@gmail.com>
2016-08-08 19:02:36 -0700

    Rename const slack bot variable to UNCLE

--- server/src/bot.js
+++ server/src/bot.js
@@ -2,16 +2,16 @@ import Botkit from 'botkit';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const controller = Botkit.slackbot({
+const UNCLE = Botkit.slackbot({
   debug: false
 });
 
-controller.spawn({
+UNCLE.spawn({
   //Create .env file in the root directory and add SLACK_BOT_TOKEN
   token: process.env.SLACK_BOT_TOKEN
 }).startRTM();
 
-controller.hears('', ['direct_message'], function(bot, message) {
+UNCLE.hears('', ['direct_message'], function(bot, message) {
   console.log('replying to message');
   let responses = ["Yooo It's your boi JBuxxx, wats good?"]
   bot.reply(message, responses[0]);
