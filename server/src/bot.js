b589352541cbe37cfc0fd9e6ab942e4c0c8f7323
Justin Ou <justinou@Justins-MacBook-Air.local>
2016-08-10 15:48:52 -0700

    Removing askName and askLocation from bot.js
    Moved into the bot/introduction.js for Buckley inroduction

--- server/src/bot.js
+++ server/src/bot.js
@@ -15,23 +15,7 @@ BUCKLEY.startRTM();
 
 connection.hears("", ['direct_message'], function(bot, message) {
   console.log('replying to message');
-  bot.startConversation(message, askFirstName);
 });
-
-let askFirstName = (response, convo) => {
-  convo.ask("Hi There! Nice to meet you. My Name is Buckley, What's yours?", (response, convo) => {
-    convo.say("Nice to meet you " + response.text + "!");
-    askLocation(response, convo);
-    convo.next();
-  });
-}
-
-let askLocation = (response, convo) => {
-  convo.ask("Where are you from?", (response, convo) => {
-    convo.say("I heard that " + response.text + " is a great place. Well I'll be here to help you out if you need me!")
-    convo.next();
-  });
-}
 
 export default BUCKLEY;
 
