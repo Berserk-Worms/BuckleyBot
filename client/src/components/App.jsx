34f343c58e2de8ac48e5388df851cbc1ea790f12
Jeff Korcal <jkorcal@gmail.com>
2016-08-11 19:57:41 -0700

    fix - change robot image to point to interal png

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -7,7 +7,7 @@ class App extends Component {
         <div className="center-vertical-align">
         <h1 className="title center-item">BuckleyBot: Slackbot for Students</h1>
           <div className="center-item">
-            <img className="robo-big-head-img" src="http://plainicon.com/dboard/userprod/2802_db2aa/prod_thumb/plainicon.com-45277-512px.png" alt=""/>
+            <img className="robo-big-head-img" src="../assets/robo_big_head.png" alt=""/>
           </div>
           <div className="slack-button center-item">
             <a href="https://slack.com/oauth/reflow?scope=bot&client_id=66765912757.67864241282&redirect_uri=http://localhost:8080/slack/teams/auth">
