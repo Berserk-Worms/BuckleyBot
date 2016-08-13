92d1fd652b50f6dc39c49cc57c22689e2d27c327
Jeff Korcal <jkorcal@gmail.com>
2016-08-12 17:26:42 -0700

    feat - add responsive navbar with working signin button

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import Navigation from './Navigation';
 
 class App extends Component {
   render() {
     return (
       <div>
+        <Navigation />
         {this.props.children}
       </div>
     );
