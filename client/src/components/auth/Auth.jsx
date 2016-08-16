47a89074ccd2f83312f8f305561928d2573fc827
Jeff Korcal <jkorcal@gmail.com>
2016-08-16 12:01:08 -0700

    refactor auth higher order component to use browser history

--- client/src/components/auth/Auth.jsx
+++ client/src/components/auth/Auth.jsx
@@ -1,21 +1,13 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { browserHistory } from 'react-router'; 
 
 export default function(ComposedComponent) {
   class Authentication extends Component {
-    static contextTypes = {
-      router: React.PropTypes.object
-    }
 
     componentWillMount() {
       if (!this.props.authenticated) {
-        this.context.router.push('/');
-      }
-    }
-
-    componentWillUpdate(nextProps) {
-      if (!nextProps.authenticated) {
-        this.context.router.push('/');
+        browserHistory.push('/');
       }
     }
 
