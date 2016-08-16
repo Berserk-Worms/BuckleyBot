36665f42fd1fe6716f5dae38f0608a0087b65061
Pranay M <pranay@utexas.edu>
2016-08-15 20:51:10 -0700

    flesh out profile page comments

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -5,6 +5,18 @@ import UserInfo from './UserInfo';
 import JobList from './JobList';
 
 class Profile extends Component {
+
+  componentWillMount() {
+    // Grab jwt from localstorage, check which user it is
+
+    // If proper user, then make API request to slack with user's access token
+    // Else messed up jwt -- maybe need to clear jwt and redirect to splash page
+
+    // Get response data and populate components with that data
+    // If data doesn't respond properly, then that means we have a faulty access token
+    // Then use refresh token to get new access token 
+  }
+
   render() {
     return (
       <Grid>
