9fa9200c3acfcf87aa3ec734dfe0dd9dd87db6bb
Pranay M <pranay@utexas.edu>
2016-08-18 15:21:45 -0700

    refactor tag query to include jobs and modularize code to be exported to jobremindercron

--- server/src/bots/job.js
+++ server/src/bots/job.js
@@ -10,54 +10,55 @@ let userJobsListener = {
     //let userId = message.user
     Tag.findOne({
       // TODO: Figure out how to make this a join table
-      where: { name: 'javascript' }
+      where: { name: 'javascript' },
+      include: [{ model: Job }],
     })
     .then((tag) => {
       if (tag) {
-        tag.getJobs()
-        .then((jobs) => {
+        //TODO Filter with sequelize rather than _.filter
+        let sample = this.returnJobSample(tag.jobs, 3);
 
-          //TODO Filter with sequelize rather than _.filter
-          let filterJobs = _.filter(jobs, (job) => {
-            //TODO: Need to do further filtering to ensure that the 
-            //user's saved job does not show up in slack
-            
-            return job.dataValues.title.toLowerCase().indexOf('lead') === -1 && 
-              job.dataValues.title.toLowerCase().indexOf('senior') === -1 && 
-              job.dataValues.title.toLowerCase().indexOf('manager') === -1 &&
-              job.dataValues.title.toLowerCase().indexOf('sr.') === -1
-          });
-          console.log(filterJobs.length);
-
-
-          //Format job data for Slack message attachment 
-          let attachments = _.map(filterJobs, (job) => {
-            return {
-              title: `:computer: ${job.dataValues.title}`,
-              text: `:office: ${job.dataValues.company} - ${job.dataValues.location} \n :link: ${job.dataValues.link}`,
-              callback_id: `clickSaveJobs`,
-              attachment_type: `default`,
-              actions: [
-                {name: `saveJob`, text: `Save`, value: job.dataValues.id, type: `button`, style: `default`}
-              ]
-            }
-          });
-
-          //Set attachment to message to be three random jobs
-          let reply_with_attachments = {
-            text: 'Here are some jobs:',
-            attachments: _.sample(attachments, 3)
-          };
-
-          bot.reply(message, reply_with_attachments);
-        })
+        //Set attachment to message to be three random jobs
+        let reply_with_attachments = {
+          text: 'Here are some jobs:',
+          attachments: sample
+        };
+        bot.reply(message, reply_with_attachments);
       } else {
         bot.reply(message, "Sorry, I couldn't find any new jobs -- please check back soon :)");
       }
-    })
+    });
   },
-  findJobs: () => {
+  returnJobSample: (jobs, numberOfJobs) => {
+    let filterJobs = _.filter(jobs, (job) => {
+      //TODO: Need to do further filtering to ensure that the 
+      //user's saved job does not show up in slack
+      
+      let jobTitle = job.dataValues.title.toLowerCase();
 
+      return jobTitle.indexOf('lead') === -1 && 
+        jobTitle.indexOf('senior') === -1 && 
+        jobTitle.indexOf('manager') === -1 &&
+        jobTitle.indexOf('sr.') === -1 &&
+        jobTitle.indexOf('principal') === -1;
+    });
+    console.log("# of filtered jobs:", filterJobs.length);
+
+    //Format job data for Slack message attachment 
+    let attachments = _.map(filterJobs, (job) => {
+      return {
+        title: `:computer: ${job.dataValues.title}`,
+        text: `:office: ${job.dataValues.company} - ${job.dataValues.location} \n :link: ${job.dataValues.link}`,
+        callback_id: `clickSaveJobs`,
+        attachment_type: `default`,
+        actions: [
+          {name: `saveJob`, text: `Save`, value: job.dataValues.id, type: `button`, style: `default`}
+        ]
+      }
+    });
+
+    let sample = _.sample(attachments, numberOfJobs);
+    return sample;
   }
 };
 
--- server/src/utils/jobReminderCron.js
+++ server/src/utils/jobReminderCron.js
@@ -1,11 +1,13 @@
 import User from '../models/userModel';
+import Tag from '../models/tagModel';
+import Job from '../models/jobModel';
+import userJobsListener from '../bots/job';
 import { store } from '../bot';
 import { startConvo } from '../bots/introduction';
-
 import { CronJob } from 'cron';
 
 let jobCron = new CronJob({
-  cronTime: '00 01 20 * * 1-5',
+  cronTime: '00 30 08 * * 1-5',
   onTick: () => {
     console.log('Cron jobs to dank jobs');
     messageUsers();
@@ -21,10 +23,25 @@ let messageUsers = () => {
       const id = user.dataValues.slackUserId;
       const BUCKLEY = store[user.dataValues.slackTeamId];
 
-      BUCKLEY.startPrivateConversation({ user: id }, (err, convo) => {
-        convo.ask('Ready for this cron job?!?', (response, convo) => {
-          convo.stop();
-        });
+      Tag.findOne({
+        // TODO: Figure out how to make this a join table
+        where: { name: 'javascript' },
+        include: [{ model: Job }],
+      })
+      .then((tag) => {
+        if (tag) {
+          //Set attachment to message to be three random jobs
+          let message_with_jobs = {
+            text: 'Good morning! I found some cool jobs you might be interested in:',
+            attachments: userJobsListener.returnJobSample(tag.jobs, 3)
+          };
+          BUCKLEY.startPrivateConversation({ user: id }, (err, convo) => {
+            convo.say(message_with_jobs);
+          });
+        }
+      })
+      .catch(err => {
+        console.log('Error:', err);
       });
     });
   })
