dc958f37da0a3c0f5bf8ecca14acda8072c73ccc
Justin Ou <justinou728@gmail.com>
2016-08-17 19:32:50 -0700

    Changed number of results shown, and client Id for splash/login
    Number of results shown from bot changed back from 15 to 3

--- client/src/components/Splash.jsx
+++ client/src/components/Splash.jsx
@@ -10,7 +10,7 @@ class Splash extends Component {
             <img className="robo-big-head-img" src="../assets/robo_big_head.png" alt=""/>
           </div>
           <div className="slack-button center-item">
-            <a href="https://slack.com/oauth/reflow?scope=bot&client_id=68292478995.69302700244&redirect_uri=http://localhost:8080/slack/teams/auth">
+            <a href="https://slack.com/oauth/reflow?scope=bot&client_id=66765912757.67864241282&redirect_uri=http://localhost:8080/slack/teams/auth">
               <img alt="Add to Slack" src="https://platform.slack-edge.com/img/add_to_slack.png" srcSet="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" />
             </a>  
           </div>
