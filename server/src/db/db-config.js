8d49081706a040965f0ee67d5112190667d5d812
Billy Lan <billylan22@gmail.com>
2016-08-19 11:08:41 -0700

    configure database url

--- server/src/db/db-config.js
+++ server/src/db/db-config.js
@@ -3,10 +3,11 @@ import Sequelize from 'sequelize';
 //make path for production environment
 let databaseUrl;
 if (process.env.NODE_ENV = 'test') {
-  console.log('starting');
   databaseUrl = 'postgres://localhost:5432/uncle_test';
+} else if (process.env.NODE_ENV = 'production') {
+  databaseUrl = process.env.DATABASE_URL 
 } else {
-  databaseUrl = process.env.DATABASE_URL || 'postgres://localhost:5432/uncle';
+  databaseUrl = 'postgres://localhost:5432/uncle'
 }
 
 let host =  !!process.env.DATABASE_URL ? process.env.DATABASE_URL.split(':')[2] : 'localhost';
