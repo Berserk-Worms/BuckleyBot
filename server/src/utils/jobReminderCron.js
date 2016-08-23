2038f323349dc0ea3b56342571243b6df391f58c
Justin Ou <justinou728@gmail.com>
2016-08-23 16:34:11 -0700

    Deleted console logs
    Changed some lets to const

--- server/src/utils/jobReminderCron.js
+++ server/src/utils/jobReminderCron.js
@@ -35,15 +35,12 @@ let messageUsers = () => {
   })
   .then(users => {
     users.forEach(user => {
-      // console.log('this is the user,', JSON.stringify(user));
       let count ={};
-      // console.log('cluster fuck. ', user.dataValues);
-      let tagArr = user.dataValues.tags.map(item => {
+      const tagArr = user.dataValues.tags.map(item => {
         return item.name
       });
       const id = user.dataValues.slackUserId;
       const BUCKLEY = store[user.dataValues.slackTeamId];
-      // console.log('this is the tagArr, ', tagArr);
 
       user.dataValues.tags.forEach(tag => {
         tag.jobs.forEach(job => {
@@ -51,11 +48,8 @@ let messageUsers = () => {
           else { count[job.id] = 1; }
         })
       })
+      const keySort = Object.keys(count).sort((a, b) => { return count[b] - count[a]}).slice(0, 6);
 
-      let keySort = Object.keys(count).sort((a, b) => { return count[b] - count[a]}).slice(0, 6);
-      // console.log('this is the count,',  keySort);
-
-      // console.log('userId keySort, ', user.slackUserId, keySort);
       if (keySort.length === 0) {
         BUCKLEY.startPrivateConversation({ user: user.slackUserId }, (err, convo) =>{
           convo.say(`It seems like you don't have any tags! Please type tags and set you filters!`);
@@ -69,8 +63,7 @@ let messageUsers = () => {
         ]}
       })
       .then(jobs => {
-        console.log('this is the jobs!', jobs);
-        let message_with_jobs = {
+        const message_with_jobs = {
           text: 'Good morning! I found some cool jobs you might be interested in:',
           attachments: userJobsListener.returnJobSample(jobs, 5)
         };
