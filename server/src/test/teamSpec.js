8ee5c851560bd01c9276a0d4c267f9b82c9f1d19
Jeff Korcal <jkorcal@gmail.com>
2016-08-22 20:10:30 -0700

    start team testing

--- server/src/test/teamSpec.js
+++ server/src/test/teamSpec.js
@@ -1,20 +1,30 @@
-// import request from 'supertest';
-// import db from '../db/db-config';
-// import Team from '../models/teamModel';
-// import User from '../models/userModel';
+import request from 'supertest';
+import { expect } from 'chai';
+import db from '../db/db-config';
+import Team from '../models/teamModel';
 
-// const server = 'http://localhost:8080/';
+const server = 'http://localhost:8080/';
 
-// xdescribe('teamController', () => {
+describe('teamController', () => {
 
-//   after((done) => {
-//     db.sync({force: true})
-//     .then(() => {
-//       done();
-//     })
-//     .catch(function(err) {
-//       done(err);
-//     });
-//   });
+  after((done) => {
+    db.sync({force: true})
+    .then(() => {
+      done();
+    })
+    .catch(function(err) {
+      done(err);
+    });
+  });
 
-// })
+  describe('User Creation', () => {
+
+      it('expects 200 on successful GET request to findAllTeams', () => {
+          
+      
+      });
+
+
+
+  })
+})
