c4f69690ea5b20e97722c0d2dd3c87c970509308
Pranay M <pranay@utexas.edu>
2016-08-24 11:31:30 -0700

    start https server if on prod

--- server/src/server.js
+++ server/src/server.js
@@ -19,24 +19,53 @@ app.use(bodyParser.json());
 app.use(express.static('client'));
 
 const router = routes(app);
+
+const credentials = {
+  key: fs.readFileSync('/etc/letsencrypt/live/buckleybot.com/privkey.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/live/buckleybot.com/fullchain.pem'),
+  ca: fs.readFileSync('/etc/letsencrypt/live/buckleybot.com/chain.pem'),
+};
+
+var httpsServer = https.createServer(credentials, app);
+
 db.sync()
 .then(() => {
-  app.listen(port, () => {
-    //Emit start event;
-    app.emit('appStarted');
-    console.log('Server started on port ' + port);
-    console.log(`${process.env.NODE_ENV} database synced`);
-    //invoking teams to generate all instances of bots
-    //which exist in the database
-    teams();
-    
-    // start cron job to do daily job reminder
-    jobCron.start();
-    jobScrape.start();
-  });
+
+  // If development
+  if (process.env.NODE_ENV === 'test' || process.env.NODE_ENV === 'dev') {
+    app.listen(port, () => {
+      //Emit start event;
+      app.emit('appStarted');
+      console.log('Server started on port ' + port);
+      console.log(`${process.env.NODE_ENV} database synced`);
+      //invoking teams to generate all instances of bots
+      //which exist in the database
+      teams();
+      
+      // start cron job to do daily job reminder
+      jobCron.start();
+      jobScrape.start();
+    });
+  } else if (process.env.NODE_ENV === 'production') {
+    // if production
+    const credentials = {
+      key: fs.readFileSync('/etc/letsencrypt/live/buckleybot.com/privkey.pem'),
+      cert: fs.readFileSync('/etc/letsencrypt/live/buckleybot.com/fullchain.pem'),
+      ca: fs.readFileSync('/etc/letsencrypt/live/buckleybot.com/chain.pem'),
+    };
+
+    var httpsServer = https.createServer(credentials, app);
+
+    httpsServer.listen(httpsPort, () => {
+      teams();
+      console.log('---- HTTPS Server started on port ' + httpsPort);
+      jobCron.start();
+      jobScrape.start();
+    });
+  } 
 })
 .catch((err) => {
   console.log('Error syncing the DB', err);
-})
+});
 
 export default app;
