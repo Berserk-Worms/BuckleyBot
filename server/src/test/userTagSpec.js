6fb853c00586a91152dfb8656c20a8d643e37748
Justin Ou <justinou728@gmail.com>
2016-08-23 23:07:28 -0700

    Added tests for userTagController
    1. Added http status codes for userTagController
    2. No changes in router or server.js or userTagSpec

--- server/src/test/userTagSpec.js
+++ server/src/test/userTagSpec.js
@@ -176,7 +176,7 @@ describe('userTagController', () => {
     it('should return 500 when incorrect tag data is posted', (done) => {
       request(app)
       .post('/api/users/tags')
-      .send({ slackUserId: 'abcd', tagId: 'xxx' })
+      .send({ slackUserId: 'abcd', tagId: 3 })
       .expect(500, done);
     })
 
