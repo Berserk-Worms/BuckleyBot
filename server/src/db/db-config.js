c4c91e48853371b7f6f9ccb1670866966ddbbfd3
Pranay M <pranay@utexas.edu>
2016-08-25 15:09:41 -0700

    fix dbconfig host to remain localhost

--- server/src/db/db-config.js
+++ server/src/db/db-config.js
@@ -6,11 +6,13 @@ if (process.env.NODE_ENV === 'test') {
   databaseUrl = 'postgres://localhost:5432/uncle_test';
 } else if (process.env.NODE_ENV === 'production') {
   databaseUrl = process.env.DATABASE_URL 
+  // maps to -> postgres://ec2-user:password@localhost:5432/uncle_test
 } else {
   databaseUrl = 'postgres://localhost:5432/uncle'
 }
 
-let host =  !!process.env.DATABASE_URL ? process.env.DATABASE_URL.split(':')[2] : 'localhost';
+// let host =  !!process.env.DATABASE_URL ? process.env.DATABASE_URL.split(':')[2] : 'localhost';
+let host = 'localhost';
 
 let db = new Sequelize(databaseUrl, {
   host: host,
