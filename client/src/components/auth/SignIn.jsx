7ece42c9d74b308c6f60b8454b74e600d142c7c3
Billy Lan <billylan22@gmail.com>
2016-08-25 17:34:00 -0700

    upate redirect uri for signin

--- client/src/components/auth/SignIn.jsx
+++ client/src/components/auth/SignIn.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Grid, Row, Col, Button } from 'react-bootstrap';
 
+const BASE_URL = window.location.origin;
 
 class SignIn extends Component {
   render() {
@@ -15,7 +16,7 @@ class SignIn extends Component {
                   <div className="skills">Sign in through slack to view all of your</div>
                   <div className="skills"> saved jobs and start applying now!</div>
                   <span className="slack-btn-container">
-                    <Button className="slack-button" bsSize="large" bsStyle="danger" href="https://slack.com/oauth/authorize?scope=identity.basic,identity.email,identity.team,identity.avatar&client_id=66765912757.67864241282&redirect_uri=http%3A%2F%2Flocalhost%3A8080%2Fslack%2Fusers%2Fauth"> 
+                    <Button className="slack-button" bsSize="large" bsStyle="danger" href={`https://slack.com/oauth/authorize?scope=identity.basic,identity.email,identity.team,identity.avatar&client_id=66765912757.67864241282&redirect_uri=${BASE_URL}/slack/users/auth`}> 
                     <div className="slack-icon"></div>
                     <div className="slack-text">Sign In</div>
                     </Button> 
